282ab9efc92fc4f496a74650ed9f02e5e9aad064
Michael Giordano <maik.giordano@hotmail.com>
2023-09-17 19:02:29 -0300

    fix direction

--- ui/screens/activity/list-transportes.screen.tsx
+++ ui/screens/activity/list-transportes.screen.tsx
@@ -65,17 +65,18 @@ export const ListTransportes = ({ navigation, route }: Props) => {
 			})
 
 			let address = 'No podemos brindar la direccion.'
-			if(location != null && location.at(0) != null ){
-				address =
-				location.at(0).name? location.at(0).name : '' +
-				', ' +
-				location.at(0).city? location.at(0).city : '' +
-				', ' +
-				location.at(0).postalCode? location.at(0).postalCode : '' +
-				', ' +
-				location.at(0).region? location.at(0).region : ''
-			} 
-			
+			if (location != null && location.at(0) != null) {
+				address = location.at(0).name
+					? location.at(0).name + ', '
+					: '' + location.at(0).city
+					? location.at(0).city + ', '
+					: '' + location.at(0).postalCode
+					? location.at(0).postalCode + ', '
+					: '' + location.at(0).region
+					? location.at(0).region
+					: ''
+			}
+
 			return address
 		} catch (error) {
 			return 'No podemos brindar la direccion.'
