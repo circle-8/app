f17d7ac3b8cb2fb1e2f060a3504533c97b75042d
NewtonGauss <germanmerkel9@gmail.com>
2023-09-23 19:54:30 -0300

    Consejos from backend

--- ui/screens/learn/learn.screen.tsx
+++ ui/screens/learn/learn.screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Center, ScrollView, Text, View, useToast } from 'native-base'
-import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LoadingScreen } from '../../components/loading.component';
 import { match } from '../../../utils/either';
 import { TabRoutes } from '../../../constants/routes';
@@ -12,17 +12,7 @@ export const Learn = (navigation) => {
 	const [isLoading, setLoading] = React.useState(true)
 	const [consejos, setConsejos] = React.useState<Consejo[]>([])
 
-	const consejosMock: Consejo[] = [
-		{ id: 1, titulo: 'Consejo 1', descripcion: 'Elige productos de limpieza ecologicos o haz tus propios limpiadores con ingredientes naturales como vinagre o bicarbonato de sodio.', fechaCreacion: '20-09-2023' },
-		{ id: 2, titulo: 'Consejo 2', descripcion: 'Evita el uso de productos de plastico desechables, como botellas de agua o bolsas de plastico. Opta por alternativas reutilizables, como botellas de acero inoxidable o bolsas de tela.', fechaCreacion: '20-09-2023' },
-		{ id: 3, titulo: 'Consejo 3', descripcion: 'Elije productos con etiquetas ecologicas o certificaciones que indiquen que son sostenibles y amigables con el medio ambiente.', fechaCreacion: '20-09-2023' },
-	];
-
 	const initialLoad = async () => {
-
-		//TODO descomentar cuando este armado el back
-		
-		/*
 		const cons = await ConsejoService.getAll()
 		match(
 			cons,
@@ -33,8 +23,8 @@ export const Learn = (navigation) => {
 				toast.show({ description: err })
 				navigation.navigate(TabRoutes.home)
 			},
-		)*/
-		setConsejos(consejosMock)
+		)
+
 		setLoading(false)
 	}
 
@@ -59,7 +49,7 @@ export const Learn = (navigation) => {
 			>
 				<View style={{ flex: 1, marginRight: 10 }}>
 					<Text style={{ fontSize: 12, textAlign: 'left' }}>
-					En esta seccion encontraras consejos para aprender y mejorar tus habitos sustentables 
+					En esta seccion encontraras consejos para aprender y mejorar tus habitos sustentables
 					</Text>
 				</View>
 				<MaterialCommunityIcons name="lightbulb-on-outline" size={40} color="black" />
